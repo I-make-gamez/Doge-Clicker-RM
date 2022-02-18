877337ece34ecffaa830a20a8593caafc28917fb
I-make-gamez <ebelcher143@gmail.com>
2022-02-18 15:48:21 +0000

    vb0.32

--- java.js
+++ java.js
@@ -2,11 +2,16 @@
 const doge = document.querySelector('.doge')
 const doge2 = document.querySelector('.doge2')
 const dc = document.querySelector('.dc')
+//Upgrade DOM
+const u1 = document.querySelector('.u1')
+const dfCost = document.querySelector('.dfCost')
 //Version DOM
 const ver = document.querySelector('.vers')
 
 //MVVs
 var doco = 0;
+var clipo = 1;
+var dfPrice = 100;
 
 //Version Start
 var version = 'vb0.31'
@@ -20,10 +25,20 @@ page.onload = function(){
 //EL Start
 
 doge.addEventListener('click', function(){
-    doco += 1;
+    doco += clipo;
     dc.innerHTML = `DogeCoin: ${doco}`
     anim()
 });
+
+u1.addEventListener('click', function(){
+    if(doco >= dfPrice){
+        doco -= dfPrice;
+        dc.innerHTML = `DogeCoin: ${doco}`
+        dfprice += Math.round((dfPrice/100)*10)
+        dfCost.innerHTML = `Cost: ${dfPrice}DC`
+        clipo += 1;
+    }
+})
 
 //EL End
 
